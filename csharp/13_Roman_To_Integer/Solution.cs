ca27861e58e425e20445cfa4b0b5664a91a31f9b
timmoth <t.jones@timmoth.com>
2022-12-04 19:35:52 +0000

    Added solution for problem 13 Roman to Integer

---
+++ csharp/13_Roman_To_Integer/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Problem_13_Roman_To_Integer;
+public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        int output = 0;
+
+        var prevValue = GetValue(s[0]);
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            var c = s[i];
+            var value = GetValue(c);
+            if (prevValue < value)
+            {
+                output -= prevValue;
+            }
+            else
+            {
+                output += prevValue;
+            }
+
+            prevValue = value;
+        }
+
+        output += prevValue;
+
+        return output;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int GetValue(char c)
+    {
+        return c switch
+        {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => 0,
+        };
+    }
+}
---
+++ csharp/Tests/Problem_13_Tests.cs
@@ -0,0 +1,20 @@
+using Problem_13_Roman_To_Integer;
+
+namespace Tests
+{
+    public class Problem_13_Tests
+    {
+        [Theory]
+        [InlineData(2, "II")]
+        [InlineData(3, "III")]
+        [InlineData(9, "IX")]
+        [InlineData(12, "XII")]
+        [InlineData(27, "XXVII")]
+        [InlineData(58, "LVIII")]
+        [InlineData(1994, "MCMXCIV")]
+        public void Test1(int expected, string input)
+        {
+            Assert.Equal(expected, new Solution().RomanToInt(input));
+        }
+    }
+}
