84803b471f99c8333f7eb298e99d856d1717dec7
timmoth <t.jones@timmoth.com>
2022-12-06 19:28:11 +0000

    Added a few typescript solutions

---
+++ typescript/1523_Count_Odd_Numbers_in_an_Interval_Range.ts
@@ -0,0 +1,7 @@
+export function countOdds(low: number, high: number): number {
+    if(low % 2 == 0 && high % 2 == 0){
+        return Math.floor((high - low) / 2);
+    }
+
+    return Math.floor((high - low) / 2) + 1;
+};
---
+++ typescript/__tests__/1523_Count_Odd_Numbers_in_an_Interval_Range.tests.ts
@@ -0,0 +1,10 @@
+import {expect, test} from '@jest/globals';
+import {countOdds} from '../1523_Count_Odd_Numbers_in_an_Interval_Range';
+
+test('Example 1', () => {
+  expect(countOdds(3, 7)).toBe(3);
+});
+
+test('Example 2', () => {
+  expect(countOdds(8, 10)).toBe(1);
+});
