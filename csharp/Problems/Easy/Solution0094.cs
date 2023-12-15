892e5e17287348c7fa12e0c6e9695d1846dc6821
timmoth <t.jones@timmoth.com>
2023-12-15 22:05:56 +0000

    Added solution to 0094

---
+++ csharp/Problems/Easy/Solution0094.cs
@@ -0,0 +1,36 @@
+﻿namespace Problems.Easy
+{
+    public class Solution0094
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            var visited = new List<int>();
+            Traverse(visited, root);
+            return visited;
+        }
+
+        public static void Traverse(List<int> visited, TreeNode node)
+        {
+            if(node == null) return;
+
+            Traverse(visited, node.left);
+
+            visited.Add(node.val);
+
+            Traverse(visited, node.right);
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
